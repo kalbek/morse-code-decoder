b710483dfafd7ee359e69a9f2e59af6f36678468
kalbek <access2kaleb@gmail.com>
2023-06-14 16:27:55 +0300

    Fix linter errors.

--- decoder.rb
+++ decoder.rb
@@ -54,15 +54,15 @@ decoded_morse_char = decode_morse_char(morse_char)
 puts decoded_morse_char
 # method to decode morse word
 def decode_morse_word(morse_word)
-    decoded_word = ''
+  decoded_word = ''
   letters = morse_word.split
-  
+
   letters.each do |letter|
     decoded_letter = decode_morse_char(letter)
     decoded_word += decoded_letter
-end
+  end
 
-decoded_word
+  decoded_word
 end
 # checking word decoding
 morse_word = '.... . .-.. .-.. ---'
@@ -70,15 +70,15 @@ decoded_morse_word = decode_morse_word(morse_word)
 puts decoded_morse_word
 # method to decode morse sentense
 def decode_morse_sentence(morse_sentence)
-    decoded_sentence = ''
-    words = morse_sentence.split
-    
-    words.each do |word|
-        decoded_word = decode_morse_word(word)
-        decoded_sentence += decoded_word + ' '
-    end
-    
-    decoded_sentence.strip
+  decoded_sentence = ''
+  words = morse_sentence.split
+
+  words.each do |word|
+    decoded_word = decode_morse_word(word)
+    decoded_sentence += decoded_word + ' '
+  end
+
+  decoded_sentence.strip
 end
 
 # checking sentence decoding
